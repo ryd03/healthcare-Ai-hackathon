85ab7df1dea43cb9473d7fdcd9d56394a0d1ef5d
Rawan Darwish <rawanydarwish@gmail.com>
2024-04-21 14:42:27 +0300

    more frontend

--- app.py
+++ app.py
@@ -6,6 +6,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
